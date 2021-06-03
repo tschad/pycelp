d197423773dcce9c4d340ec89b34a6a39480211f
Tom Schad <tomschad10@gmail.com>
2021-06-03 10:00:31 -1000

    package doc update

--- pycelp/__init__.py
+++ pycelp/__init__.py
@@ -1,16 +1,4 @@
 """
-pycelp is a python package for Coronal Emission Line Polarization calculations.
-It is used to forward synthesize the polarized emission of ionized atoms
-formed in the solar corona. It calculates the atomic density matrix elements
-for a single ion under coronal equilibrium conditions and excited by a
-prescribed radiation field and thermal collisions. In its initial release,
-pyCELP solves a set of statistical equilibrium equations in the spherical
-statistical tensor respresentation for a multi-level atom for the no-coherence
-case. This approximation is useful in the case of forbidden line emission by
-visible and infrared lines, such as Fe XIII 1074.7 nm and Si X 3.9 um.
-See Schad & Dima 2020 for more details and specific references.
-
 .. include:: ../README.md
 """
-
 from .ion import Ion
