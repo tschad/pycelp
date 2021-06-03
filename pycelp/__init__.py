2ca22a8b40dc32a429a2660e2205e155196bce1d
Tom Schad <tomschad10@gmail.com>
2021-06-03 09:53:16 -1000

    doc update

--- pycelp/__init__.py
+++ pycelp/__init__.py
@@ -10,7 +10,7 @@ case. This approximation is useful in the case of forbidden line emission by
 visible and infrared lines, such as Fe XIII 1074.7 nm and Si X 3.9 um.
 See Schad & Dima 2020 for more details and specific references.
 
-.. include:: ./documentation.md
+.. include:: ./package_documentation.md
 """
 
 from .ion import Ion
