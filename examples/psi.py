c8baaa6e54add4f8cb2e37ae0b9fb4fd3d154672
tschad <tomschad10@gmail.com>
2022-02-17 10:37:39 -1000

    add dataframes to example and more psi additions

--- examples/psi.py
+++ examples/psi.py
@@ -84,56 +84,11 @@ class Model:
         self.bx   = bx
         self.by   = by
         self.bz   = bz
+        self.rlen = rlen 
+        self.blen = blen 
         self.b_localinc = localinc
-        
-        
-
-        
-
-        
-        
-        
-        
-        
-    
-    
-    
+                
 """
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import RegularGridInterpolator as rgi
-from scipy.interpolate import interp1d
-import pickle
-from scipy.spatial.transform import Rotation as R
-import multiprocessing
-import time
-
-#plt.ion()
-plt.close('all')
-
-from pyhdf.SD import SD, SDC
-
-def read_psi_hdf(fn):
-    file = SD(fn, SDC.READ)
-    dat  = file.select('Data-Set-2').get()
-    s0   = file.select('fakeDim0').get()
-    s1   = file.select('fakeDim1').get()
-    s2   = file.select('fakeDim2').get()
-    return dat,s0,s1,s2
-
-def read_interp_psi(fn,points_ref):
-    print(' read_interp_psi: ',fn)
-    ## read data and get interpolating function
-    dat,lons,lats,rs = read_psi_hdf(fn)
-    fInt = rgi((lons,lats,rs),dat,method = 'linear',fill_value = 0.,bounds_error = False)
-    ## get meshgrid of points for the new reference coordinates
-    lons_ref,lats_ref,rs_ref = points_ref
-    npoints = np.meshgrid(lons_ref,lats_ref,rs_ref,indexing = 'ij')
-    flat = np.array([m.flatten() for m in npoints])
-    ## interpolate and reshape
-    datInt = fInt(flat.T).reshape(*npoints[0].shape)
-    return datInt
 
 ## INPUTS
 obsLon     = 185.     ## observer's longitude
@@ -144,41 +99,6 @@ arcsamp    = 10.    ## sampling in arcsecond
 rsunarc    = 960.  ## radius of sun in arcseconds .. later replace with sun ephemeris
 pycle_pickle_file = './pycle_calcs/psi_pycle_outputs_fe11.pickle'
 wvAng_synth = 7892.
-
-## Interpolate PSI model onto an unstaggered grid...
-## Use lons from br and lats,rs from bp as the reference coordinates
-br,lons,latsB,rsB = read_psi_hdf('./corona/br002.hdf')
-bp,lonsB,lats,rs = read_psi_hdf('./corona/bp002.hdf')
-
-temp = read_interp_psi('./corona/t002.hdf',(lons,lats,rs))
-br = read_interp_psi('./corona/br002.hdf',(lons,lats,rs))
-bt = read_interp_psi('./corona/bt002.hdf',(lons,lats,rs))
-bp = read_interp_psi('./corona/bp002.hdf',(lons,lats,rs))
-ne = read_interp_psi('./corona/rho002.hdf',(lons,lats,rs))
-
-## have to convert MAS normalized units into physical units
-temp = temp*(2.807067e7) ##  K
-br = br*2.2068908   ## Gauss
-bt = bt*2.2068908   ## Gauss
-bp = bp*2.2068908   ## Gauss
-ne = ne*1.e8        ## cm^-3
-
-##spherical coordinates and cartesian coordinates
-phi3d,theta3d,r3d = np.meshgrid(lons,lats,rs,indexing = 'ij')
-x3d = r3d*np.sin(theta3d)*np.cos(phi3d)
-y3d = r3d*np.sin(theta3d)*np.sin(phi3d)
-z3d = r3d*np.cos(theta3d)
-
-## cartesian vectors of the magnetic field
-bx = br*np.sin(theta3d)*np.cos(phi3d) + bt*np.cos(theta3d)*np.cos(phi3d) - bp*np.sin(phi3d)
-by = br*np.sin(theta3d)*np.sin(phi3d) + bt*np.cos(theta3d)*np.sin(phi3d) + bp*np.cos(phi3d)
-bz = br*np.cos(theta3d)               - bt*np.sin(theta3d)
-
-## get inclinations in local solar reference frame
-## using dot product between local radial and the magnetic field
-rlen = np.sqrt(x3d**2 + y3d**2 + z3d**2)
-blen = np.sqrt(bx**2 + by**2 + bz**2)
-localinc  = np.arccos(((bx*x3d + by*y3d + bz*z3d)/(rlen*blen)).clip(min = -1,max=1))
 
 ## this should be enough to get the alignments and rho_00 by using PYCLE
 ## someday may try to make this into a lookup table version
