bbf80f93039e736b3c5e4967290a0bb5c20ebd81
tschad <tomschad10@gmail.com>
2022-02-18 16:03:03 -1000

    close to doing integration seting up geometry

--- examples/psi.py
+++ examples/psi.py
@@ -73,103 +73,48 @@ class Model:
         blen = np.sqrt(bx**2 + by**2 + bz**2)
         localinc  = np.arccos(((bx*x3d + by*y3d + bz*z3d)/(rlen*blen)).clip(min = -1,max=1))
 
+        ## Grid sample locations 
         self.lons = lons
         self.lats = lats 
-        self.rs = rs 
+        self.rs   = rs
+        
+        ## cartesian coordinats
+        self.x3d = x3d 
+        self.y3d = y3d 
+        self.z3d = z3d 
+        
         self.temp = temp 
+        self.ne   = ne
+        
+        ## spherical magnetic field components
         self.br   = br
         self.bt   = bt
         self.bp   = bp
-        self.ne   = ne
+        
+        ## cartesian magnetic field components
         self.bx   = bx
         self.by   = by
         self.bz   = bz
-        self.rlen = rlen 
-        self.blen = blen 
-        self.b_localinc = localinc
-                
-"""
 
-## INPUTS
-obsLon     = 185.     ## observer's longitude
-b0         = -5.57    ## observer's heliographic latitude -- b0 angle
-Obs_Sun_AU = 1.     ## replace with ephermeris data
-fov_rsun   = 6.     ## +/- 3 rsun
-arcsamp    = 10.    ## sampling in arcsecond
-rsunarc    = 960.  ## radius of sun in arcseconds .. later replace with sun ephemeris
-pycle_pickle_file = './pycle_calcs/psi_pycle_outputs_fe11.pickle'
-wvAng_synth = 7892.
-
-## this should be enough to get the alignments and rho_00 by using PYCLE
-## someday may try to make this into a lookup table version
-pht     = rlen.flatten() - 1
-petemp  = temp.flatten()
-pedens  = ne.flatten()
-pbgaus  = blen.flatten()
-pbthet  = np.rad2deg(localinc).flatten()
-save_var = pht,petemp,pedens,pbgaus,pbthet
-pickle_file = './psi_model_params4pycle.pickle'
-pickle.dump(save_var,open(pickle_file,"wb"))
-
-# pickle_file = './psi_model_params4pycle.pickle'
-# pht,petemp,pedens,pbgaus,pbthet  = pickle.load(open(pickle_file,"rb"))
-#  >>> GO RUN psi_pycle_mpi.py on DSSC cluster for different ions
-# this will give me the alignments, etc....
-
-##################
-## recall the simulation geometry and plot
-## compare to GONG maps of the CR 2189
-
-rObs     = Obs_Sun_AU * (1.495978707e11/6.96340e8)
-thetaObs = np.pi/2. - np.deg2rad(b0)
-phiObs   = np.deg2rad(obsLon)
-xObs,yObs,zObs = rObs*np.sin(thetaObs)*np.cos(phiObs),rObs*np.sin(thetaObs)*np.sin(phiObs),rObs*np.cos(thetaObs)
-
-lonsd = np.rad2deg(lons)
-latsd = np.flip(np.rad2deg(lats)-90.)  ## flip as in observers frame angles increase towards north
-extrad = (lons[0],lons[-1],lats[-1],lats[0])
-extdeg = (lonsd[0],lonsd[-1],latsd[-1],latsd[0])
-fig,ax = plt.subplots(nrows = 3,ncols = 1,figsize = (6,8))
-ax = ax.flatten()
-ax[0].imshow(br[:,:,0].T,extent = extrad)
-ax[1].imshow(br[:,:,0].T,extent = extdeg)
-ax[2].imshow(z3d[:,:,0].T,extent = extrad)
-ax[0].plot(np.zeros(1)+phiObs,np.zeros(1)+thetaObs,'x',markersize = 5,color= 'black')
-ax[1].plot(np.zeros(1)+phiObs,np.zeros(1)+thetaObs,'x',markersize = 5,color= 'black')
-
-## setup the synthesized field of view
-
-yarc = np.linspace(-fov_rsun/2.*rsunarc,fov_rsun/2.*rsunarc,np.int(np.ceil(fov_rsun*rsunarc/arcsamp)))
-zarc = yarc
-yya,zza  = np.meshgrid(yarc,zarc,indexing = 'ij')
-rra = np.sqrt(yya**2. + zza**2.)
-m_behind_sun = 1.*(rra>rsunarc)
-
-################
-## define new coordinate system with x towards
-## observer, and z towards north pole
-## get points in the plane
-
-xxObs = np.zeros_like(yya)
-yyObs = rObs * np.tan(np.deg2rad(yya/3600.))
-zzObs = rObs * np.tan(np.deg2rad(zza/3600.))
-
-## rotate these points into the model geometry with Euler rotation
-r = R.from_euler('yz',[-(thetaObs-np.pi/2.),-(phiObs)])
-b = np.stack((xxObs.flatten(),yyObs.flatten(),zzObs.flatten()))
-xyz_model = np.matmul(r.as_matrix(), b)
-plt.imshow(xyz_model[1,:].reshape(xxObs.shape).T,origin = 'lower')
-
-## NOW WITH THE XYZ_MODEL POINTS AND THE LOCATION OF THE OBSERVER,
-## come up with the parametric equations for the los of sight
-## and then get the spherical coordinates, interpolate for rho and temps, etc.
-## synthesize and integrate for integrated I,Q,U, (V?)
-
-losvec = np.stack((xObs-xyz_model[0,:],yObs-xyz_model[1,:],zObs - xyz_model[2,:]))
-losveclen = np.linalg.norm(losvec,axis=0,ord=2,keepdims = True)
-losvec = losvec / losveclen
-startpt = np.stack((xyz_model[0,:],xyz_model[1,:],xyz_model[2,:]))
-
-
- 
-"""
+        ## Field magnitude and local frame inclination angle 
+        self.bmag = blen 
+        self.thetaBlocal = localinc
+        
+    def __repr__(self):
+         return f"""psi Model class
+    ---------------------
+    Data Directory Names: {self.dir}
+    Number of longitude samples: {len(self.lons)}
+    Number of latitude samples: {len(self.lats)}
+    Number of radial samples: {len(self.rs)}
+    Data shape: {self.temp.shape}
+    
+    Variables: 
+    lons -- Longitudes
+    lats -- Latitudes
+    rs   -- Radial samples 
+    br,bt,bp  -- Spherical components of magnetic field 
+    bx,by,bz  -- Cartesian components of magnetic field 
+    bmag      -- total magnetic field intensity
+    thetaBlocal -- location inclination of magnetic field in solar frame
+    """
